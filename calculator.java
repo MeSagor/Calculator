b67b32830e8f5f636d4a7959512f4eff2f8270f6
MeSagor <dear.shorifulislam@gmail.com>
2021-11-15 22:42:13 +0600

    added subtrack function

--- calculator.java
+++ calculator.java
@@ -4,6 +4,10 @@ public class calculator {
         return (a + b);
     }
 
+    static int subtrackIt(int a, int b) {
+        return (a - b);
+    }
+
     public static void main(String[] args) {
         //   System.out.println("Hello World");
         int firstNum = 50;
