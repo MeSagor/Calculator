d007eef7da35835877dbf2cfe67590250ff01380
MeSagor <dear.shorifulislam@gmail.com>
2021-11-15 15:54:01 +0600

    A basic sum programm in java

---
+++ calculator.java
@@ -0,0 +1,14 @@
+public class calculator {
+
+    static int sumIt(int a, int b) {
+        return (a + b);
+    }
+
+    public static void main(String[] args) {
+        //   System.out.println("Hello World");
+        int firstNum = 50;
+        int secondNum = 30;
+        int sum = sumIt(firstNum, secondNum);
+        System.out.println(sum);
+    }
+}
