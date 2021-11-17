96f626a19e4d20c05ef2ea88608326b326f45907
Ashiq Uddin Pranto <ashiqpranto@gmail.com>
2021-11-17 21:01:29 +0600

    Modified the whole calculator program & added functionality for user to choose option

--- calculator.java
+++ calculator.java
@@ -1,23 +1,89 @@
+import java.util.Scanner;
+
 public class calculator {
 
-    static int sumIt(int a, int b) {
-        return (a + b);
+    static void addition() {
+
+        int a,b;
+        System.out.println("Enter 2 integer number for addition:\n");
+        
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println("Result = "+(a+b));
     }
 
-    static int subtrackIt(int a, int b) {
-        return (a - b);
+    static void subtraction() {
+
+        int a,b;
+        System.out.println("Enter 2 integer number for Subtraction:\n");
+        
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+    
+        System.out.println("Result = "+(a-b));
     }
     
-    static int division(int a, int b){
-        return a/b;
+    static void division(){
+
+       int a,b;
+        System.out.println("Enter 2 integer number for Division:\n");
+        
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt(); 
+        
+        System.out.println("Result = "+(a/b));
         // please add try-catch because if b = 0, the program will crash.
     }
+    static void multiplicatoin()
+    {
+        int a,b;
+        System.out.println("Enter 2 integer number for addition:\n");
+        
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
 
+        System.out.println("Result = "+(a*b));
+
+    }
     public static void main(String[] args) {
         //   System.out.println("Hello World");
-        int firstNum = 50;
-        int secondNum = 30;
-        int sum = sumIt(firstNum, secondNum);
-        System.out.println(sum);
+
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter Your Choice: ");
+
+            System.out.println("'1' for Addition");
+            System.out.println("'2' for Subtraction");
+            System.out.println("'3' for Multiplication");
+            System.out.println("'4' for Division");
+            System.out.println("'0' for exit");
+            int choice = sc.nextInt();
+            
+            switch (choice) {
+                case 1:
+                    addition();                
+                    break;
+                case 2:
+                    subtraction();
+                    break;
+                case 3:
+                    multiplicatoin();
+                    break;
+                case 4:
+                    division();
+                    break;
+                case 0:
+                    return;
+                default:
+                    break;
+            }
+        }
+        
     }
 }
